19c8548b67ddaa0b32315e01915f5ae4b540b501
Jonathan Salazar <jonathan96@unitec.edu>
2019-04-29 17:00:34 -0600

    Chapter 9 - Layouts, Partials and View Helpers completed

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   get 'demo/hello'
   get 'demo/other_hello'
   get 'demo/google'
+  get 'demo/escape_output'
   #default routing
   # get ':controller(/:action(/:id))'
 
